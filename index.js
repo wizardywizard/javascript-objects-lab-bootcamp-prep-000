fbbbdfeba1435267c47caaed735a1d87b3b7692c
David Little <littledavid175@gmail.com>
2019-06-02 19:34:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object
 }
 function deleteFromObjectByKey(object,Key) {
-  return Object.assign({},object)
+  return Object.assign({},{})
   
 }
