2bc3be93d3792c52d05d69c4bee147854d01a3be
David Little <littledavid175@gmail.com>
2019-06-02 19:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes={food:"bread"};
 function updateObjectWithKeyAndValue(object,key,value){
    return Object.assign({},object,{[key]:value})
 }
-function destructivelyUpdateObjectWithKeyAndValue(){
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   
   
 }
